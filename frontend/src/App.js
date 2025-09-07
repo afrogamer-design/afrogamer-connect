5b7f5f511e9fb63386c5accd4aa07e82110fe733
afrogamer-design <nyolagift77@gmail.com>
2025-09-07 17:27:21 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+// frontend/src/App.js
+import React from '
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SupabaseProvider } from './contexts/SupabaseContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <SupabaseProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </SupabaseProvider>
   );
 }
 
