60f7e9e9b6b3be42fa04c20f49dea54025358a83
afrogamer-design <nyolagift77@gmail.com>
2025-09-07 17:34:39 +0300

    Delete backend directory

--- backend/models/db.js
+++
@@ -1,9 +0,0 @@
-import pkg from "pg";
-const { Pool } = pkg;
-
-const pool = new Pool({
-  connectionString: "postgresql://postgres:jCZSQrkX54ACP7PI@db.kodicbboapdsowvgicmx.supabase.co:5432/postgres",
-  ssl: { rejectUnauthorized: false },
-});
-
-export default pool;
